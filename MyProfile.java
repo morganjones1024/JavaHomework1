7d3e093384d1322be6e2ee76756c6855240a3576
morganjones <morganjones@Morgans-Air.attlocal.net>
2022-03-28 16:05:40 -0400

    homework

---
+++ MyProfile.java
@@ -0,0 +1,10 @@
+
+public class MyProfile {
+	public static void main(String[] args) {   
+		  String name = "Morgan";
+		  int age = 26;
+		  double desiredSalary = 125000.00;
+		  char gender = 'f';
+		  boolean lookingForJob = true;
+			}
+}
